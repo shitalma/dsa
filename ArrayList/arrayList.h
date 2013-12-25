9199d709d09e4869eb3dc8d66e0cba032e17e85b
unknown <shitalma@Inshitalma.corporate.thoughtworks.com>
2013-12-25 12:16:35 +0530

    with external iterator

--- ArrayList/arrayList.h
+++ ArrayList/arrayList.h
@@ -1,13 +1,23 @@
+#include "iterator.h"
 typedef struct {
-	void** base;
-	int capacity;
-	int length;
+        void** base;
+        int capacity;
+        int length;
 } ArrayList;
 
 ArrayList create(int capacity);
+typedef int (*compare)(void* element1,void *element2);
 
 int insert(ArrayList *list, int index, void* data);
 
+int remove(ArrayList *list,int index);
+
+int add(ArrayList *list,void *data);
+
 void* get(ArrayList *list, int index);
+
+int search(ArrayList *list, void *searchElement,compare cmpFun);
+
+Iterator getIterator(ArrayList* list);
 
 void dispose(ArrayList *list);
