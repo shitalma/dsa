6c448e748434a805fdaa8937897be593252f7eaf
unknown <shitalma@Inshitalma.corporate.thoughtworks.com>
2013-12-30 00:05:22 +0530

    with refactoring code

--- merge-sort/mergeSort.c
+++ merge-sort/mergeSort.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-int merge(void** base , void** left , void** right , int leftlength , int rightlength,
-		int elementsize , compare comp){
+
+void merge(void** base,void** left,void** right,int leftlength,int rightlength,
+	compare comp){
 	int i = 0 , j = 0 , k = 0;
-	while(i < leftlength && j < rightlength){
+	for(;i < leftlength && j < rightlength;k++){
 		if(comp(left[i],right[i])){
 			base[k] = left[i];
 			i++;
@@ -14,35 +15,26 @@ int merge(void** base , void** left , void** right , int leftlength , int rightl
 			base[k] = right[j];
 			j++;
 		}
-		k++;
 	}
-	while(j < rightlength){
+	for(;j < rightlength; j++,k++)
 		base[k] = right[j];
-		j++;
-		k++;
-	}
-	while(i < leftlength){
+	for(;i < leftlength;i++,k++)
 		base[k] = left[i];
-		i++;
-		k++;
-	}
-	return 1;
 }
+
 void msort(void** base, int numberOfElements,int elementSize,compare comp) {
-    int mid = numberOfElements / 2,leftlength,rightlength;
-    int i,j;
+    int mid = numberOfElements / 2,leftlength = mid,
+    	rightlength = numberOfElements - mid,i;
     void** left = calloc(mid,elementSize);
     void** right = calloc((numberOfElements-mid),elementSize);
     if(numberOfElements < 2) return;
-    leftlength = mid;
-    rightlength = numberOfElements - mid;
-    for (i = 0; i < leftlength ; i++)
-    	left[i] = base[i];
-    for (i = mid; i < numberOfElements ; i++)
-    	right[i-mid] = base[i];
+
+    for (i = 0; i < leftlength ; i++)  left[i] = base[i];
+    for (i = mid; i < numberOfElements ; i++)  right[i-mid] = base[i];
+    	
     msort(left , leftlength , elementSize , comp);
     msort(right, rightlength, elementSize, comp);
-    merge(base,left,right,leftlength,rightlength,elementSize,comp);
+    merge(base,left,right,leftlength,rightlength,comp);
     free(left);
     free(right);
 }
