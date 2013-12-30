bd146c2c44a70e271579688807a52d8566cf89e0
unknown <shitalma@Inshitalma.corporate.thoughtworks.com>
2013-12-30 14:09:36 +0530

    working for the get method properly

--- hashmap/hashmap.c
+++ hashmap/hashmap.c
@@ -4,11 +4,11 @@
 #include "internalHashMap.h"
 #include <stdlib.h>
 #include <stdio.h>
-// void createListForEachBucket(void *bucket){
-// 	DoubleList list;
-// 	list  = dList_create();
-// 	*(DoubleList*)bucket = list;
-// }
+void createListForEachBucket(void *bucket){
+	DoubleList list;
+	list  = dList_create();
+	*(DoubleList*)bucket = list;
+}
 HashMap HashMap_createMap(hash hashFunc, compare compareKey){
 	HashMap map;
 	int i;
@@ -19,7 +19,7 @@ HashMap HashMap_createMap(hash hashFunc, compare compareKey){
 	map.hashFunc = hashFunc;
 	for(i = 0;i < 10;i++)
 		ArrayList_add(map.buckets, malloc(sizeof(DoubleList)));
-	// ArrayList_iterate(*(ArrayList*)map.buckets, createListForEachBucket);
+	ArrayList_iterate(*(ArrayList*)map.buckets, createListForEachBucket);
 	return map;
 }
 HashNode* HashMap_createHashNode(void *key, void *value){
@@ -39,31 +39,34 @@ int HashMap_put(HashMap *map, void *key, void *value){
 	return 1;
 }
 void* HashMap_get(HashMap *map, void *key){
-	int bucketNumber;
-	DoubleList *list;
-	HashNode *hash_node;
-	bucketNumber = (map->hashFunc(key)) % 10;
-	list = (DoubleList*)ArrayList_get(map->buckets, bucketNumber);
-	hash_node = dList_getData(*list, key, map->cmp);
-	if(hash_node)
-		return hash_node->value;
-	return hash_node;
+    node* Node;
+    HashNode* data;
+    int i;
+    int bucketNumber = map->hashFunc(key) % 10; 
+    DoubleList *list = (DoubleList*)ArrayList_get(map->buckets, bucketNumber);
+    if(0 == list->length) return NULL;
+    Node = (node*)list->head;
+    for(i=0;i<list->length;i++){
+            data = (HashNode*)Node->data;
+        if (!map->cmp(key ,data->key)) return data->value;
+        Node = Node->next;
+    }
+    return NULL;
 }
-// 	it = dList_getIterator(list);
-// 	while(it.hasNext(&it)){
-// 		hash_node = it.next(&it);
-// 		if(0 == map->cmp(hash_node->key,key))
-// 		 	break;	
-// 		index++;
-// 	}
-// 	if(index == list->length)
-// 		return 0;
-// 	return dList_delete(list, index);
-// }
+// void* HashMap_get(HashMap *map, void *key){
+// 	int bucketNumber;
+// 	DoubleList *list;
+// 	HashNode *hash_node;
+// 	bucketNumber = (map->hashFunc(key)) % 10;
+// 	list = (DoubleList*)ArrayList_get(map->buckets, bucketNumber);
 
+// 	hash_node = dList_getData(*list, key, map->cmp);
+// 	if(hash_node)
+// 		return hash_node->value;
+// 	return hash_node;
+// }
 int HashMap_remove(HashMap* map, void* key){
-	int bucketNumber;
-	int index = 0;
+	int bucketNumber,index = 0;
 	DoubleList* list;
 	HashNode* hash_node;
 	Iterator it;
@@ -78,4 +81,15 @@ int HashMap_remove(HashMap* map, void* key){
 	}
 	if(index == list->length) return 0;
 	return dList_delete(list,index);
+}
+void* HashMap_keys(HashMap *map){
+	Iterator Arrayiterator = ArrayList_getIterator(map->buckets);
+	Iterator listIterator;
+	HashNode hash_node;
+	while(Arrayiterator.hasNext(&Arrayiterator)){
+		listIterator = dList_getIterator(Arrayiterator.next(&Arrayiterator));
+		while(listIterator.hasNext(&listIterator))
+			hash_node = (*(HashNode*)listIterator.next(&listIterator));
+	}
+	return hash_node.key;
 }
--- hashmap/hashmapTest.c
+++ hashmap/hashmapTest.c
@@ -1,7 +1,7 @@
 #include "testUtils.h"
 #include "hashmap.h"
 #include <string.h>
-
+#include <stdio.h>
 //create setup, tearDown, fixtureSetup, fixtureTearDown methods if needed
 
 int areKeyEqual(void* key1 , void* key2 ){
@@ -15,8 +15,11 @@ typedef struct{
 	char value[50];
 } Intern;
 Intern manali = {15441,"Manali"};
-Intern shital = {15442,"Shital"};
+Intern shital = {15445,"Shital"};
 Intern kajal = {15345,"Kajal"};
+Intern sayali = {15349,"Sayali"};
+Intern tanbir = {15348,"Tanbir"};
+
 
 void test_add_an_element_to_hashmap(){
 	HashMap map = HashMap_createMap(hashFun, areKeyEqual);
@@ -47,4 +50,18 @@ void test_remove_data_from_hashmap_when_data_not_present(){
 	HashMap map = HashMap_createMap(hashFun, areKeyEqual);
 	ASSERT(HashMap_put(&map, &shital.key, &shital.value));
 	ASSERT(0 == HashMap_remove(&map, &manali.key));
+}
+void test_keys_of_hashmap_gives_all_keys_in_map(){
+	HashMap map = HashMap_createMap(hashFun, areKeyEqual);
+	Iterator it;	
+	void* result;
+	int key1 = 67809 , value1 = 6;
+	ASSERT(HashMap_put(&map, &shital.key, &shital.value));
+	ASSERT(HashMap_put(&map, &manali.key, &manali.value));
+	ASSERT(HashMap_put(&map, &kajal.key, &kajal.value));
+	ASSERT(HashMap_put(&map, &sayali.key, &sayali.value));
+	ASSERT(HashMap_put(&map, &key1, &value1));
+	result = HashMap_keys(&map);
+	printf("%d\n",*(int*)result );
+	// HashMap_keys(&map);
 }
